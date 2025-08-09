92bb5590130bf8d20836b26a483798479d527381
Vedantbaliyan <154498885+Vedantbaliyan@users.noreply.github.com>
2025-08-09 19:15:30 +0530

    Updated frontend and admin

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -11,7 +11,7 @@ const AdminContextProvider=(props)=>{
     const [appointments,setAppointments]=useState([])
     const [dashData,setDashData]=useState(false)
 
-    const backendUrl = import.meta.env.VITE_BACKEND_URL
+    const backendUrl = "https://prescriptobyvedant.onrender.com"
 
     const getAllDoctors=async()=>{
         try {
